8a89bc223d0b54cbee53f5538bf99d8cdfb34db2
Hudson Reis <hudsonpr09@gmail.com>
2025-06-16 13:43:19 -0300

    Finalizando Desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Olá, Github!");
+        Scanner input = new Scanner(System.in);
+
+        // Numero da Conta
+        int acountNumber = (int) (Math.random() * 100);
+
+        // Agencia
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agency = input.next();
+
+        // Nome do Cliente
+        System.out.print("\nPor favor, digite o nome do Cliente: ");
+        String name = input.next();
+
+        // Saldo Depositado
+        System.out.print("\nPor favor, digite o saldo a ser depositado: ");
+        double balance = input.nextDouble();
+        double teste = 100;
+
+        // Mensagem final
+        System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque", name, agency, acountNumber, balance);
     }
 }
